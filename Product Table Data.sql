fbb1298d1fdf59977b726b087dc921a9fd354d24
kemarkhan <149648225+kemarkhan@users.noreply.github.com>
2024-12-25 17:47:03 +0800

    Add files via upload

---
+++ Product Table Data.sql
@@ -0,0 +1,36 @@
+-- Insert data into Product Table
+INSERT INTO Product (Product_ID, Product_Name, Category) VALUES ('1', 'Chicken Breast', 'Meats');
+INSERT INTO Product (Product_ID, Product_Name, Category) VALUES ('2', 'Tomato Sauce', 'Sauces');
+INSERT INTO Product (Product_ID, Product_Name, Category) VALUES ('3', 'Mushrooms', 'Vegetables');
+INSERT INTO Product (Product_ID, Product_Name, Category) VALUES ('4', 'Coconut Milk', 'Dairy');
+INSERT INTO Product (Product_ID, Product_Name, Category) VALUES ('5', 'Rice', 'Grains');
+INSERT INTO Product (Product_ID, Product_Name, Category) VALUES ('6', 'Spaghetti Pasta', 'Grains ');
+INSERT INTO Product (Product_ID, Product_Name, Category) VALUES ('7', 'Chilli Sauce', 'Sauces');
+INSERT INTO Product (Product_ID, Product_Name, Category) VALUES ('8', 'Bolognese Sauce', 'Sauces');
+INSERT INTO Product (Product_ID, Product_Name, Category) VALUES ('9', 'Olive Oil', 'Oils ');
+INSERT INTO Product (Product_ID, Product_Name, Category) VALUES ('10', 'Garlic', 'Vegetables');
+INSERT INTO Product (Product_ID, Product_Name, Category) VALUES ('11', 'Onion', 'Vegetables');
+INSERT INTO Product (Product_ID, Product_Name, Category) VALUES ('12', 'Cheddar Cheese', 'Dairy ');
+INSERT INTO Product (Product_ID, Product_Name, Category) VALUES ('13', 'Parmesan Cheese', 'Dairy ');
+INSERT INTO Product (Product_ID, Product_Name, Category) VALUES ('14', 'Eggs', 'Dairy ');
+INSERT INTO Product (Product_ID, Product_Name, Category) VALUES ('15', 'Bread', 'Baking');
+INSERT INTO Product (Product_ID, Product_Name, Category) VALUES ('16', 'Lettuce', 'Vegetables');
+INSERT INTO Product (Product_ID, Product_Name, Category) VALUES ('17', 'Tomato', 'Vegetables');
+INSERT INTO Product (Product_ID, Product_Name, Category) VALUES ('18', 'Mustard', 'Sauces');
+INSERT INTO Product (Product_ID, Product_Name, Category) VALUES ('19', 'Vinegar', ' Vinegars');
+INSERT INTO Product (Product_ID, Product_Name, Category) VALUES ('20', 'Butter', 'Baking');
+INSERT INTO Product (Product_ID, Product_Name, Category) VALUES ('21', 'Bacon', 'Meats');
+INSERT INTO Product (Product_ID, Product_Name, Category) VALUES ('22', 'Ham', 'Meats');
+INSERT INTO Product (Product_ID, Product_Name, Category) VALUES ('23', 'Flour', 'Baking');
+INSERT INTO Product (Product_ID, Product_Name, Category) VALUES ('24', 'Butter', 'Dairy');
+INSERT INTO Product (Product_ID, Product_Name, Category) VALUES ('25', 'Sugar', 'Baking');
+INSERT INTO Product (Product_ID, Product_Name, Category) VALUES ('26', 'Milk', 'Dairy ');
+INSERT INTO Product (Product_ID, Product_Name, Category) VALUES ('27', 'Banana', 'Fruits');
+INSERT INTO Product (Product_ID, Product_Name, Category) VALUES ('28', 'Tea Bags', ' Tea');
+INSERT INTO Product (Product_ID, Product_Name, Category) VALUES ('29', 'Coffee Beans', 'Coffee');
+INSERT INTO Product (Product_ID, Product_Name, Category) VALUES ('30', 'Apple', 'Fruits');
+
+
+
+
+
