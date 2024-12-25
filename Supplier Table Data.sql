c705e86202f4006693ab39f25e9a7e4e50f72086
kemarkhan <149648225+kemarkhan@users.noreply.github.com>
2024-12-25 17:41:04 +0800

    Add files via upload

---
+++ Supplier Table Data.sql
@@ -0,0 +1,57 @@
+--SUPPLIER TABLE DATA
+
+INSERT INTO Supplier (Supplier_ID, Supplier_Name, Supplier_PhoneNum, Supplier_Email, Supplier_Address) 
+VALUES (1, 'Fresh Poultry Supplier', '013-467-9873', 'contact@poultrysupplier.com', '200 Poultry Ave, Johor Bahru, Johor, Malaysia');
+
+INSERT INTO Supplier (Supplier_ID, Supplier_Name, Supplier_PhoneNum, Supplier_Email, Supplier_Address) 
+VALUES (2, 'Sauces Co', '012-479-5678', 'sales@saucesandspices.com', '400 Sauce Street, Penang, Malaysia');
+
+INSERT INTO Supplier (Supplier_ID, Supplier_Name, Supplier_PhoneNum, Supplier_Email, Supplier_Address) 
+VALUES (3, 'Vegetable Harvesters', '019-874-2894', 'info@vegmushroomharvesters.com', '600 Fungus Way, Kuala Lumpur, Malaysia');
+
+INSERT INTO Supplier (Supplier_ID, Supplier_Name, Supplier_PhoneNum, Supplier_Email, Supplier_Address) 
+VALUES (4, 'Coconut Products Co.', '014-503-4612', 'support@coconutdairyproducts.com', '800 Coconut Drive, Selangor, Malaysia');
+
+INSERT INTO Supplier (Supplier_ID, Supplier_Name, Supplier_PhoneNum, Supplier_Email, Supplier_Address) 
+VALUES (5, 'Grains Distributors', '018-390-2267', 'info@grainsdistributors.com', '900 Grain Plaza, Perak, Malaysia');
+
+INSERT INTO Supplier (Supplier_ID, Supplier_Name, Supplier_PhoneNum, Supplier_Email, Supplier_Address) 
+VALUES (6, 'Italian Pasta Co.', '017-654-3900', 'contact@italianpastasauces.com', '1100 Pasta Lane, Johor, Malaysia');
+
+INSERT INTO Supplier (Supplier_ID, Supplier_Name, Supplier_PhoneNum, Supplier_Email, Supplier_Address) 
+VALUES (7, 'Chilli Sauce Suppliers', '013-334-4215', 'sales@chilliandhotsauces.com', '1200 Spice Court, Melaka, Malaysia');
+
+INSERT INTO Supplier (Supplier_ID, Supplier_Name, Supplier_PhoneNum, Supplier_Email, Supplier_Address) 
+VALUES (8, 'Bolognese Sauce Producers', '016-678-9003', 'sales@bolognesesauces.com', '1300 Italian District, Kuala Lumpur, Malaysia');
+
+INSERT INTO Supplier (Supplier_ID, Supplier_Name, Supplier_PhoneNum, Supplier_Email, Supplier_Address) 
+VALUES (9, 'Olive Oil Co.', '015-557-8834', 'contact@oliveoilvinegar.com', '1400 Olive Road, Penang, Malaysia');
+
+INSERT INTO Supplier (Supplier_ID, Supplier_Name, Supplier_PhoneNum, Supplier_Email, Supplier_Address) 
+VALUES (10, 'Fresh Ltd.', '018-231-1098', 'support@garliconion.com', '1500 Fresh Street, Melaka, Malaysia');
+
+INSERT INTO Supplier (Supplier_ID, Supplier_Name, Supplier_PhoneNum, Supplier_Email, Supplier_Address) 
+VALUES (11, 'Dairy Suppliers', '016-667-3926', 'sales@dairycheesesuppliers.com', '1600 Dairy Crescent, Kuala Lumpur, Malaysia');
+
+INSERT INTO Supplier (Supplier_ID, Supplier_Name, Supplier_PhoneNum, Supplier_Email, Supplier_Address) 
+VALUES (12, 'Eggs Products', '010-100-0010', 'info@eggsbakery.com', '1700 Egg Park, Johor Bahru, Johor, Malaysia');
+
+INSERT INTO Supplier (Supplier_ID, Supplier_Name, Supplier_PhoneNum, Supplier_Email, Supplier_Address) 
+VALUES (13, 'Tomato Suppliers', '018-876-6543', 'contact@tomatogrowers.com', '1800 Red Farm, Penang, Malaysia');
+
+INSERT INTO Supplier (Supplier_ID, Supplier_Name, Supplier_PhoneNum, Supplier_Email, Supplier_Address) 
+VALUES (14, 'Flour Mills', '012-299-6654', 'sales@flourandsugar.com', '1900 Milling Road, Melaka, Malaysia');
+
+INSERT INTO Supplier (Supplier_ID, Supplier_Name, Supplier_PhoneNum, Supplier_Email, Supplier_Address) 
+VALUES (15, 'Butter Producers', '011-145-8753', 'support@buttermargarine.com', '2000 Dairy Circle, Selangor, Malaysia');
+
+
+
+
+
+
+
+
+
+
+
